f9ad64b2fc977835e2cb89d332dfdc62d64dd599
Kyle Knobloch <kyleknobloch@duxbury.k12.ma.us>
2015-12-08 15:23:16 -0500

    Initial Code Cleanup and more move to PhP

---
+++ DuxburyRobotics.php
@@ -0,0 +1,109 @@
+<?php
+$domain = $_SERVER['SERVER_NAME'];
+if($domain == 'git.duxburyrobotics.com') {
+    header('Location: http://git.duxburyrobotics.com:81');
+}
+
+error_reporting(0); //Cuz' fuck em.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Duxbury High School Robotics Team">
+    <meta name="author" content="@Fredghostkyle for DHS Robitics Team">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Duxbury Robotics</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <style>
+        .carousel img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            min-width: 100%;
+            height: 500px;
+        }
+    </style>
+
+</head>
+
+<!-- NAVBAR
+================================================== -->
+<div id=""></div>
+<body><!--
+    <div class="navbar-wrapper">
+      <div class="container">-->
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><!--<img src="img/dragon.png" width="20px">-->  Duxbury Robotics</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/team">Our Team</a></li>
+                <li class="dropdown">
+                    <a href="/media">Media <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="/media#Gallery">Gallery</a></li>
+                        <li><a href="/media#Videos">Videos</a></li>
+                        <li><a href="/media#archives">Archives</a></li>
+                    </ul>
+                </li>
+                <li><a href="/sponsors"><span class="glyphicon glyphicon-heart-empty"></span> Sponsors</a></li>
+                <li class=""><a href="/engineering"><span class="glyphicon glyphicon-apple"></span> Engineering Week</a></li>
+                <!--                <li><a href="blog/">Blog</a></li>-->
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="social"><a href="http://twitter.com/DuxburyRobotics"><span class="glyphicons social-twitter"></span></a> </li>
+                <li class="social"><a href="http://fb.com/DuxburyRobotics"><span class="glyphicons social-facebook"></span></a></li>
+                <li class="social"><a href="http://instagram.com/DuxburyRobotics"><span class="glyphicons social-instagram"></span> </a> </li>
+                <li class="social"> <a href="/contact"> <span class="glyphicons social-e-mail"></span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+
+    <?php require_once "module/$module.php"; ?>
+
+    <!-- FOOTER -->
+
+
+    <hr>
+    <footer>
+        <p class="pull-right">FIRST Team 4908 | <a href="/schoology">Schoology</a></p>
+        <p>Copyright © Duxbury Robotics </p>
+    </footer>
+
+</div><!-- /.container -->
+
+
+<script src="/js/jquery.min.js"></script>
+<script src="/js/bootstrap.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="/js/ie10-viewport-bug-workaround.js"></script>
+
+
+</body>
+</html>
---
+++ module/index.php
@@ -0,0 +1,74 @@
+<div class="well">
+<center>
+    <h2>Duxbury Robotics</h2>
+    <p>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe src="https://player.vimeo.com/video/130433669" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/130433669">Robotics Documentary</a> from <a href="https://vimeo.com/dragontv">Dragon TV</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+    </div>
+    </p>
+    <br>
+    <a class="btn btn-primary btn-lg" role="button" href="/sponsorus">Sponsor Us</a>
+</center>
+</div> <!-- end div in DuxburyRobotics.php -->
+
+<br>
+<br>
+<br>
+
+<div class="container marketing">
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <img src="/img/teambanner.JPG">
+                <center>
+                    <div class="caption">
+                        <h3>Team 4908</h3>
+                        <p>We are not a team, we are a family. </p>
+                        <p><a href="/team" class="btn btn-primary" role="button">Learn More</a></p>
+                        <hr>
+                        <h3>FIRST Robotics</h3>
+                        <p>We're a FIRST Robotics team. </p>
+                        <p><a href="/FIRST" class="btn btn-primary" role="button">Learn More</a></p>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <center>
+                    <div class="caption">
+                        <h3>Recent Updates</h3>
+                        <p>
+                            <a class="twitter-timeline" href="https://twitter.com/DuxburyRobotics" data-widget-id="556475594922532864">Tweets by @DuxburyRobotics</a>
+                            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                        </p>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <img src="/img/shirt.JPG">
+                <center>
+                    <div class="caption">
+                        <h3>Our sponsors</h3>
+                        <p>Power to the team. </p>
+                        <p><a href="/sponsors" class="btn btn-primary" role="button">Learn More</a></p>
+                        <hr>
+                        <h3>Sponsor Us </h3>
+                        <p>Robotics is expensive. We operate on a tight budget. With your help, we can achieve so much more.</p>
+                        <p><a href="/sponsorus" class="btn btn-primary" role="button">Sponsor Us</a></p>
+                    </div>
+                </center>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <hr>
+
+    <center>
+        <p><a href="http://www.nasa.gov"><img src="/img/sponsor/nasa.png" width="10%"></a> &emsp;&emsp; <a href="http://ptc.com"><img src="/img/sponsor/ptc.png" width="19%"></a> <a href="http://www.conwayins.com/"><img src="/img/sponsor/conway.png" width="25%"></a><a href="http://www.trieng.com/"><img src="/img/sponsor/tri.png" width="24%"></a> <a href="http://www.duxburyeducationfoundation.org/"><img src="/img/DEF.png"></a></p>
+        <p></p>
+    </center>
