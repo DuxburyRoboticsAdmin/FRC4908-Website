580d5144ce79ecd8a7537efe36de52cf34fc8eb1
fredghostkyle <fredghostkyle@yahoo.com>
2015-12-15 17:44:28 -0500

    Added an album to the archive

--- module/archives.php
+++ module/archives.php
@@ -13,11 +13,10 @@
                 <a data-flickr-embed="true" data-header="true" data-footer="true"  href="https://www.flickr.com/photos/125918646@N04/albums/72157662118043441" title="Instagram"><img src="https://farm6.staticflickr.com/5571/14839126885_def5dc0c2e_z.jpg" width="640" height="640" alt="Instagram"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script><br>
             </div>
             <div class="tab-pane fade" id="2014tab">
-                <a data-flickr-embed="true" data-header="true" data-footer="true"  href="https://www.flickr.com/photos/125918646@N04/albums/72157662375129745" title="Robotics Test"><img src="https://farm1.staticflickr.com/704/23099435533_c2bea1f383.jpg" width="640" height="640" alt="Robotics Test"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+                <a data-flickr-embed="true" data-header="true" data-footer="true"  href="https://www.flickr.com/photos/138474364@N02/albums/72157662271757992" title="Boston University Competition 2014"><img src="https://farm1.staticflickr.com/662/23692020521_868aaa944f.jpg" width="500" height="333" alt="Boston University Competition 2014"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
                 <br>
                 <iframe class="" width="560" height="315" src="//www.youtube.com/embed/td5CGlTd9tc" frameborder="0" allowfullscreen></iframe>
                 <br>
-                <iframe class="" width="560" height="315" src="//www.youtube.com/embed/td5CGlTd9tc" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
