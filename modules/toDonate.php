7f6f78f8a020d77b0891c1dd0ca2c2ad22356e14
fredghostkyle <fredghostkyle@yahoo.com>
2016-01-19 16:05:21 -0500

    Removed a .

--- modules/toDonate.php
+++ modules/toDonate.php
@@ -39,7 +39,7 @@
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                     <input type="hidden" name="cmd" value="_s-xclick">
                     <input type="hidden" name="hosted_button_id" value="2MJ8X4QYKG5TQ">
-                    <button name="submit" class="btn btn-info btn-lg">Donate via. PayPal</button>
+                    <button name="submit" class="btn btn-info btn-lg">Donate via PayPal</button>
                     <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
                 </form>
             </div>
