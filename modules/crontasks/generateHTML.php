bcd1b00a3d72bf6578c6e17a98daa7d8dc68587b
fredghostkyle <fredghostkyle@yahoo.com>
2016-01-28 10:09:59 -0500

    We're going to keep the files that change blank on the git. They will have to generate on the server... shit.

--- modules/crontasks/generateHTML.php
+++ modules/crontasks/generateHTML.php
@@ -35,106 +35,91 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 else {
-
-    //TODO: Change from echo to fprint.
-
-
     ini_set("default_charset", "UTF-8");
 
-    $table = $TableMember;
+    while (!$MemberFinish || !$TeamLeaderFinish || !$MentorFinish || !$StudentAlumFinish || !$MentorAlumFinish) {
 
-    $sql = "SELECT ID,Type,Name,Position,Year,Bio,Picture FROM " . $table;
-    $result = $conn->query($sql);
+        if (!$MemberFinish) {
+            $table = $TableMember;
+            $file = $MembersFile;
+            $errorFoundIn = "Members";
+            $MemberFinish = true;
+        } else if (!$TeamLeaderFinish) {
+            $table = $TableTeamLeader;
+            $file = $TeamLeaderFile;
+            $errorFoundIn = "Team Leaders";
+            $TeamLeaderFinish = true;
+        } else if (!$MentorFinish) {
+            $table = $TableMentor;
+            $file = $MentorFile;
+            $errorFoundIn = "Mentors";
+            $MentorFinish = true;
+        } else if (!$MentorAlumFinish) {
+            $table = $TableMentorAlumni;
+            $file = $MembersFile;
+            $errorFoundIn = "Mentor Alumni";
+            $MentorAlumFinish = true;
+        } else if (!$StudentAlumFinish) {
+            $table = $TableStudentAlumni;
+            $file = $StudentAlumFile;
+            $errorFoundIn = "Student Alumni";
+            $StudentAlumFinish = true;
+        }
 
-    if ($result->num_rows > 0) {
-        $count = 0;
+        $sql = "SELECT ID,Type,Name,Position,Year,Bio,Picture FROM " . $table;
+        $result = $conn->query($sql);
 
-        fwrite($MembersFile, '<div class="row">');
+        if ($result->num_rows > 0) {
+            $count = 0;
 
-        while($row = $result->fetch_assoc()) {
+            fwrite($file, '<div class="row">');
 
-            if ($count == 3){
-                fwrite($MembersFile, '</div><div class="row"');
+            while ($row = $result->fetch_assoc()) {
+
+                if ($count == 3) {
+                    fwrite($file, '</div><div class="row"');
+                }
+
+                //ID & start
+                fwrite($file, '<!-- ID:' . $row['ID'] . '} Type: ' . $row['Type'] . '--><div class="col-lg-4">
+                               <div class="panel panel-default">
+                               <div class="panel-heading clearfix">');
+
+                //Name
+                fwrite($file, '<h4 class="panel-title" style="text-align: center"><strong>' . $row['Name'] . '</strong>');
+
+                //img
+                fwrite($file, '</div><img src="' . $row['Picture'] . '" class="img-responsive"/>');
+
+                //Position
+                fwrite($file, '<div class="list-group">
+                               <div class="list-group-item">
+                               <h4 class="list-group-item-heading">' . $row['Position'] . '</h4>');
+
+                //Year
+                fwrite($file, '<h4 class="list-group-item-heading">' . $row['Year'] . '</h4></div>');
+
+                //Bio & end
+                fwrite($file, '<div class="list-group-item">
+                               <p class="list-group-item-text">' . $row['Bio'] . '</p>
+                               </div>
+                               </div>
+                               </div>
+                               </div>');
+
+                $count++;
+                //echo $count; //DEBUG
+
             }
 
-            //ID & start
-            fwrite($MembersFile, '<!-- ID:'.$row['ID']. '} Type: '.$row['Type'].'--><div class="col-lg-4">
-            <div class="panel panel-default">
-            <div class="panel-heading clearfix">');
+            //echo $count; //DEBUG
+            fclose($file);
 
-            //Name
-            fwrite($MembersFile, '<h4 class="panel-title" style="text-align: center"><strong>'.$row['Name'].'</strong>');
-
-            //img
-            fwrite($MembersFile, '</div><img src="'.$row['Picture'].'" class="img-responsive"/>');
-
-            //Position
-            fwrite($MembersFile, '<div class="list-group">
-            <div class="list-group-item">
-                <h4 class="list-group-item-heading">'.$row['Position'].'</h4>');
-
-            //Year
-            fwrite($MembersFile, '<h4 class="list-group-item-heading">'.$row['Year'].'</h4></div>');
-
-            //Bio & end
-            fwrite($MembersFile, '<div class="list-group-item">
-                <p class="list-group-item-text">'.$row['Bio'].'</p>
-                </div>
-                </div>
-                </div>
-                </div>');
-
-            $count++;
-            //echo $count;
+        } else { //error
+            echo '<p>Currently there are no ' . $errorFoundIn . '. (Something went wrong, no rows were found...)</p><br>';
         }
-        //echo $count;
-        fclose($MembersFile);
-    } else { //error
-        echo 'Currently there are no members. (Something went wrong, no rows were found...)';
+
     }
 
-
-
-    /*
-    <div class="col-lg-4">
-        <div class="panel panel-default">
-            <div class="panel-heading clearfix">
-                <h4 class="panel-title" style="text-align: center"><strong>Kyle Knobloch</strong>
-            </div>
-            <img src="http://www.teamstudio.com/Portals/218295/images/istock_000001242290small.jpg" class="img-responsive"/>
-            <div class="list-group">
-                <div class="list-group-item">
-                    <h4 class="list-group-item-heading">Lead Webmaster and Marketing</h4>
-                    <h4 class="list-group-item-heading">Softmore</h4>
-                </div>
-                <div class="list-group-item">
-                    <p class="list-group-item-text">BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH
-                        BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH
-                        BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH vvvBLAH BLAH v
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-     */
-
-    //|| !$TeamLeaderFinish || !$MentorFinish || !$StudentAlumFinish || !$MentorAlumFinish
-
-
-
-    /*$sql = "SELECT ID,Name FROM " . $DBTable;
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo $row['ID']. ' = ' . $row['Name']. '<br>';
-
-            echo '';
-
-        }
-    } else {
-        echo '0 results';
-    }*/
-    echo '<h1>Duxbury Robotics HTML Code Generation.</h1><p>finished.</p>';
+    echo '<h1>Duxbury Robotics HTML Code Generation.</h1><p>finished. (you can hit the back button now).</p>';
 }
