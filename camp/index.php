d947d7c68488dd989cf863e81c540e5a00a0e44c
fredghostkyle <fredghostkyle@yahoo.com>
2016-02-02 13:13:18 -0500

    Camp update.

---
+++ camp/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Duxbury Robotics</title>
+
+    <link rel="icon" href="/favicon.ico">
+
+    <!--
+        CSS
+    -->
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <link rel="stylesheet" href="/css/bootstrapunited.css">
+    <link rel="stylesheet" href="/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="/css/robotics.css">
+    <link rel="stylesheet" href="/css/bootcards-desktop.css">
+
+</head>
+
+<body>
+<!-- Google tracking -->
+<?php include_once("../js/analyticstracking.php") ?>
+<!--/Google tracking -->
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid  navbar-static-top">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse">
+
+            <a href="/" class="navbar-brand">Duxbury Robotics</a>
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Team <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <!--<li><a href="/team.php">Meet The Team</a></li>
+                        <li><a href="/alumni.php">Alumni</a></li>
+                        <li><a href="/robots.php">Robots</a> </li>
+                        <li><a href="/gallery.php">Gallery</a> </li>-->
+                        <li><a href="/about.php">About</a> </li>
+                        <li><a href="/contact.php">Contact Us</a> </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sponsor <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/sponsors.php">Sponsors</a> </li>
+                        <li><a href="/sponsorus.php">Sponsor Us</a> </li>
+                    </ul>
+                </li>
+                <!--<li><a class="nav-link" href="/events.php">Events</a></li>-->
+                <li class="dropdown">
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contribute <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/donate.php">Donate</a></li>
+                        <li><a href="/volunteer.php">Volunteer</a></li>
+                    </ul>
+                </li>
+                <li class="active"><a href="/camp"><strong>Camp</strong></a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="http://blog.duxburyrobotics.com">Blog</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <br>
+    <div class="jumbotron">
+        <h1 class="lead" style="text-align: center">Duxbury Robotics Camp</h1>
+        <p>This February, the Duxbury High School Robotics Club will be holding a three day camp for Alden students to
+            teach them the basics of programming, building, and teamwork through the creation of Lego Mindstorms Ev3
+            robots. The camp will be between 2:30 and 4:30pm after school Wednesday, Thursday, and Friday on February
+            24th to 26th in the Alden School Cafeteria. The camp is open to 3rd through 5th graders. Snacks will be provided!
+        </p>
+        <br>
+        <p style="text-align: center"><strong>Open to all Alden Elementary Students</strong></p>
+        <p style="text-align: center"><strong>Wednesday, February 24th – Friday, February 26th, 2:30-4:30pm</strong></p>
+        <p style="text-align: center"><strong>Located in the Alden Cafeteria</strong></p>
+        <br>
+        <p style="text-align: center"><strong>Sign-up form located below.</strong></p>
+        <p style="text-align: center">Please fill out this form for each student you're signing up.</p>
+        <br>
+        <p style="text-align: center">If you have any questions, comments or concerns please feel free to <a href="../contact.php">Contact Us</a>.</p>
+    </div>
+    <div class="container">
+        <center><iframe src="https://docs.google.com/forms/d/1nyg5Wk25a9gw0EZ96qFts0q1hcI37XPiyjEqZi2jzLg/viewform" width="80%" height="1500" frameborder="false"></iframe></center>
+    </div>
+</div>
+
+<footer>
+    <hr />
+    <div class="container">
+        <p class="">Copyright &copy; Duxbury Robotics 2013-2016 | <a href="http://admin.duxburyrobotics.com/"><span class="mdi mdi-speedometer"></span> Administration</a> </p>
+        <p></p>
+    </div>
+</footer>
+
+<!--
+    JavaScript
+-->
+<script src="/js/jquery-1.11.3.min.js"></script>
+<script src="/js/bootstrap.js"></script>
+<script src="/js/parallax.min.js"></script>
+<script src="/js/robotics.js"></script>
+
+
+</body>
+
+</html>
--- modules/index.php
+++ modules/index.php
@@ -13,6 +13,9 @@
                 We are currently in our third year as the team, and we now have almost 40 members.
             </strong>
         </p>
+        <div class="alert alert-success">
+            <p style="text-align: center">We're hosting the <i>Duxbury Robotics Camp</i> for Alden Students, learn more <a href="/camp" style="display:inline;">here</a>!</p>
+        </div>
         <!--<dic class="row">
             <div class="col-lg-11">
                 <p class="lead">Duxbury Robotics is committed to helping promote STEM education. With your help you can
