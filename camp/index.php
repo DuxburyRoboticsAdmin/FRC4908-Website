c6b27c85d9878450a243e339c75ddeaf3430e9b3
fredghostkyle <fredghostkyle@yahoo.com>
2016-02-02 14:55:16 -0500

    Fee update.

--- camp/index.php
+++ camp/index.php
@@ -11,10 +11,6 @@
 
     <link rel="icon" href="/favicon.ico">
 
-    <!--
-        CSS
-    -->
-
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -94,6 +90,9 @@
         <p style="text-align: center"><strong>Open to all Alden Elementary Students</strong></p>
         <p style="text-align: center"><strong>Wednesday, February 24th – Friday, February 26th, 2:30-4:30pm</strong></p>
         <p style="text-align: center"><strong>Located in the Alden Cafeteria</strong></p>
+        <br>
+        <p style="text-align: center"><strong>Fee is $30</strong></p>
+        <p style="text-align: center">Please make checks payable to <i>Duxbury Robotics</i></p>
         <br>
         <p style="text-align: center"><strong>Sign-up form located below.</strong></p>
         <p style="text-align: center">Please fill out this form for each student you're signing up.</p>
