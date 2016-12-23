d8a2e3db6e97eff4e8c0dd53fbfbe5bef5c03d79
fredghostkyle <fredghostkyle@yahoo.com>
2016-12-22 20:48:59 -0500

    WIP: Major updates for the FLL and FRC

--- modules/FIRST_FRC.php
+++ modules/FIRST_FRC.php
@@ -6,7 +6,7 @@
 
 <section class="section content" id="FRC">
     <div class="container">
-        <h2 style="text-align: center">About us</h2>
+        <h1 style="text-align: center">About us</h1>
         <div class="row">
             <div class="col-md-4"><br><img src="../img/field.JPG" class="img-rounded img-responsive" width="180px"></div>
             <div class="col-md-8"><h2>Origins</h2>
@@ -32,7 +32,114 @@
             </div>
         </div>
     </div>
+
+    <br>
+    <br>
+
+    <section class="section parallax parallax-FIRST-FRC-robots" id="home">
+        <div class="container">
+            <h1>Robots</h1>
+        </div>
+    </section>
+
+
     <div class="container">
         <!-- robot tabs by year. 2014-2016 -->
+        <div>
+            <br>
+            <br>
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+                <!--<li role="presentation" class=""><a href="#love" aria-controls="home" role="tab" data-toggle="tab">2016</a></li>-->
+                <li role="presentation" class="active"><a href="#16" aria-controls="home" role="tab" data-toggle="tab">2016</a></li>
+                <li role="presentation"><a href="#15" aria-controls="profile" role="tab" data-toggle="tab">2015</a></li>
+                <li role="presentation"><a href="#14" aria-controls="messages" role="tab" data-toggle="tab">2014</a></li>
+            </ul>
+
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <!-- 2016 -->
+                <div role="tabpanel" class="tab-pane active" id="16">
+                    <h2>Strong Hold</h2>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="/img/robot1.JPG" class="image-responsive">
+                        </div>
+                        <div class="col-md-8">
+                            <p>Talk about robot and game that year. Success fail? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/VqOKzoHJDjA" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-4">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 2015 -->
+                <div role="tabpanel" class="tab-pane fade" id="15">
+                    <div role="tabpanel" class="tab-pane active" id="16">
+                        <h2>Recycle Rush</h2>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="/img/robot1.JPG" class="image-responsive">
+                            </div>
+                            <div class="col-md-8">
+                                <p>Talk about robot and game that year. Success fail? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/mAN1B7oKDXE" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-md-4">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- 2014 -->
+                <div role="tabpanel" class="tab-pane fade" id="14">
+                    <div role="tabpanel" class="tab-pane active" id="16">
+                        <h2>Arial Assist</h2>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="/img/robot1.JPG" class="image-responsive">
+                            </div>
+                            <div class="col-md-8">
+                                <p>Talk about robot and game that year. Success fail? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/oxp4dkMQ1Vo" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-md-4">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
     </div>
 </section>
